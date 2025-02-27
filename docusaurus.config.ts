b207a7eb95c74f490fdd4a2e27c5575245cb0806
NicholasGoh <gohn0004@e.ntu.edu.sg>
2025-02-28 00:13:07 +0800

    build(mermaid): add mermaid cli rendering for editing mdx

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,6 +5,11 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
