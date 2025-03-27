d3f323370951508188c8ba178df865d3e9845ef5
NicholasGoh <gohn0004@e.ntu.edu.sg>
2025-03-27 16:33:56 +0800

    chore(homepage): point to resume
    will add resume at a later time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,14 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+         <a
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+            href="Nicholas-Goh-Resume.pdf"
+            target="_blank"
+            download={"Nicholas-Goh-Resume"}
+          >
+            Resume
+          </a>
         </div>
       </div>
     </header>
