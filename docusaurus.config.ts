ef79f8b7924fb8966dca0b018d6b0c9a27fe34cf
NicholasGoh <gohn0004@e.ntu.edu.sg>
2025-03-29 13:26:01 +0800

    chore(docusaurus): default dark theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,6 +70,9 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      defaultMode: "dark",
+    },
     navbar: {
       title: "Nicholas Goh",
       logo: {
